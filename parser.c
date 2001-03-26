01861538346e86b7974082a050c150d0f78c321a
Daniel Veillard <veillard@src.gnome.org>
2001-03-26 22:51:03 +0000

    parser.c: fixed line number reporting on error Daniel

--- parser.c
+++ parser.c
@@ -1595,7 +1595,7 @@ xmlPushInput(xmlParserCtxtPtr ctxt, xmlParserInputPtr input) {
  */
 int
 xmlParseCharRef(xmlParserCtxtPtr ctxt) {
-    int val = 0;
+    unsigned int val = 0;
     int count = 0;
 
     if (ctxt->token != 0) {
@@ -4129,6 +4129,8 @@ void
 xmlParseCharData(xmlParserCtxtPtr ctxt, int cdata) {
     const xmlChar *in;
     int nbchar = 0;
+    int line = ctxt->input->line;
+    int col = ctxt->input->col;
 
     SHRINK;
     GROW;
@@ -4182,6 +4184,8 @@ xmlParseCharData(xmlParserCtxtPtr ctxt, int cdata) {
 	} while ((*in >= 0x20) && (*in <= 0x7F));
 	nbchar = 0;
     }
+    ctxt->input->line = line;
+    ctxt->input->col = col;
     xmlParseCharDataComplex(ctxt, cdata);
 }
 

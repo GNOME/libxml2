6426935a9a40b617f1c98219c990bcd6ff2666a1
Daniel Veillard <veillard@src.gnome.org>
2001-05-04 17:52:34 +0000

    - HTMLparser.c: fixed htmlNewDoc SYSTEM and PUBLIC ID inversion   when both parameters are NULL. Daniel

--- HTMLparser.c
+++ HTMLparser.c
@@ -1813,8 +1813,8 @@ htmlDocPtr
 htmlNewDoc(const xmlChar *URI, const xmlChar *ExternalID) {
     if ((URI == NULL) && (ExternalID == NULL))
 	return(htmlNewDocNoDtD(
-		    BAD_CAST "-//W3C//DTD HTML 4.0 Transitional//EN",
-		    BAD_CAST "http://www.w3.org/TR/REC-html40/loose.dtd"));
+		    BAD_CAST "http://www.w3.org/TR/REC-html40/loose.dtd",
+		    BAD_CAST "-//W3C//DTD HTML 4.0 Transitional//EN"));
 
     return(htmlNewDocNoDtD(URI, ExternalID));
 }

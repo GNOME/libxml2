9a89a8ab80bc021deec9da7d79bda20995c4e78d
Daniel Veillard <veillard@src.gnome.org>
2001-06-27 11:13:35 +0000

    moved the symlinks detection within a CVS check, this is not portable and
    * configure.in: moved the symlinks detection within a CVS
      check, this is not portable and will be removed soon.
    * xpath.c: small cleanup/speedup
    Daniel

--- xpath.c
+++ xpath.c
@@ -5556,7 +5556,8 @@ xmlXPathParseNCName(xmlXPathParserContextPtr ctxt) {
 	while (((*in >= 0x61) && (*in <= 0x7A)) ||
 	       ((*in >= 0x41) && (*in <= 0x5A)) ||
 	       ((*in >= 0x30) && (*in <= 0x39)) ||
-	       (*in == '_'))
+	       (*in == '_') || (*in == '.') ||
+	       (*in == '-'))
 	    in++;
 	if ((*in == ' ') || (*in == '>') || (*in == '/') ||
             (*in == '[') || (*in == ']') || (*in == ':') ||

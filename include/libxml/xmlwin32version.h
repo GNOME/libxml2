04382ae3d6fbca19e73220337512a7e1c21c002f
Daniel Veillard <veillard@src.gnome.org>
2001-09-12 18:51:30 +0000

    preparing for 2.4.4 updated and rebuilt the docs Daniel
    * configure.in: preparing for 2.4.4
    * doc/xml.html doc/html/*: updated and rebuilt the docs
    Daniel

--- include/libxml/xmlwin32version.h
+++ include/libxml/xmlwin32version.h
@@ -27,21 +27,21 @@ extern void xmlCheckVersion(int version);
  *
  * the version string like "1.2.3"
  */
-#define LIBXML_DOTTED_VERSION "2.4.3"
+#define LIBXML_DOTTED_VERSION "2.4.4"
 
 /**
  * LIBXML_VERSION:
  *
  * the version number: 1.2.3 value is 1002003
  */
-#define LIBXML_VERSION 20403
+#define LIBXML_VERSION 20404
 
 /**
  * LIBXML_VERSION_STRING:
  *
  * the version number string, 1.2.3 value is "1002003"
  */
-#define LIBXML_VERSION_STRING "20403"
+#define LIBXML_VERSION_STRING "20404"
 
 /**
  * LIBXML_TEST_VERSION:
@@ -49,7 +49,7 @@ extern void xmlCheckVersion(int version);
  * Macro to check that the libxml version in use is compatible with
  * the version the software has been compiled against
  */
-#define LIBXML_TEST_VERSION xmlCheckVersion(20403);
+#define LIBXML_TEST_VERSION xmlCheckVersion(20404);
 
 #if 0
 /**

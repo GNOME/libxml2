f7b094f7ee2a9a7f2f9f05c7390a199c950ec131
Daniel Veillard <veillard@src.gnome.org>
2001-11-15 13:54:39 +0000

    use the URL notation file:// for default catalog paths Daniel
    * catalog.c: use the URL notation file:// for default catalog paths
    Daniel

--- catalog.c
+++ catalog.c
@@ -55,10 +55,10 @@
 #define XML_URN_PUBID "urn:publicid:"
 #define XML_CATAL_BREAK ((xmlChar *) -1)
 #ifndef XML_XML_DEFAULT_CATALOG
-#define XML_XML_DEFAULT_CATALOG "/etc/xml/catalog"
+#define XML_XML_DEFAULT_CATALOG "file:///etc/xml/catalog"
 #endif
 #ifndef XML_SGML_DEFAULT_CATALOG
-#define XML_SGML_DEFAULT_CATALOG "/etc/sgml/catalog"
+#define XML_SGML_DEFAULT_CATALOG "file:///etc/sgml/catalog"
 #endif
 
 static int xmlExpandCatalog(xmlCatalogPtr catal, const char *filename);

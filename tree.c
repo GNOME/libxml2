4cbe470258d586294877083ca7f5aa95638f9bd5
Daniel Veillard <veillard@src.gnome.org>
2002-05-05 06:57:27 +0000

    modified xmlNodeSetBase to allow changing the base of a document. Daniel
    * tree.c: modified xmlNodeSetBase to allow changing the
      base of a document.
    Daniel

--- tree.c
+++ tree.c
@@ -3670,11 +3670,9 @@ xmlNodeSetBase(xmlNodePtr cur, xmlChar* uri) {
         case XML_TEXT_NODE:
         case XML_CDATA_SECTION_NODE:
         case XML_COMMENT_NODE:
-        case XML_DOCUMENT_NODE:
         case XML_DOCUMENT_TYPE_NODE:
         case XML_DOCUMENT_FRAG_NODE:
         case XML_NOTATION_NODE:
-        case XML_HTML_DOCUMENT_NODE:
         case XML_DTD_NODE:
         case XML_ELEMENT_DECL:
         case XML_ATTRIBUTE_DECL:
@@ -3685,13 +3683,25 @@ xmlNodeSetBase(xmlNodePtr cur, xmlChar* uri) {
 	case XML_NAMESPACE_DECL:
 	case XML_XINCLUDE_START:
 	case XML_XINCLUDE_END:
-#ifdef LIBXML_DOCB_ENABLED
-	case XML_DOCB_DOCUMENT_NODE:
-#endif
 	    return;
         case XML_ELEMENT_NODE:
         case XML_ATTRIBUTE_NODE:
 	    break;
+        case XML_DOCUMENT_NODE:
+#ifdef LIBXML_DOCB_ENABLED
+	case XML_DOCB_DOCUMENT_NODE:
+#endif
+        case XML_HTML_DOCUMENT_NODE: {
+	    xmlDocPtr doc = (xmlDocPtr) cur;
+
+	    if (doc->URL != NULL)
+		xmlFree((xmlChar *) doc->URL);
+	    if (uri == NULL)
+		doc->URL = NULL;
+	    else
+		doc->URL = xmlStrdup(uri);
+	    return;
+	}
     }
     
     ns = xmlSearchNsByHref(cur->doc, cur, XML_XML_NAMESPACE);

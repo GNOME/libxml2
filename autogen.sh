830e8971f4f63557df0b0ef48a6d7baf31fdaa0e
Havoc Pennington <hp@pobox.com>
2002-08-18 22:22:13 +0000

    hardcode aclocal-1.4/automake-1.4 so that users with both automake 1.6 and
    2002-08-18  Havoc Pennington  <hp@pobox.com>
    
    	* autogen.sh: hardcode aclocal-1.4/automake-1.4 so that users with
    	both automake 1.6 and 1.4 installed get the right automake. Means
    	compilation from CVS will now require the latest automake 1.4
    	release, or manually creating symlinks called "automake-1.4" and
    	"aclocal-1.4"

--- autogen.sh
+++ autogen.sh
@@ -24,7 +24,7 @@ DIE=0
 	DIE=1
 }
 
-(automake --version) < /dev/null > /dev/null 2>&1 || {
+(automake-1.4 --version) < /dev/null > /dev/null 2>&1 || {
 	echo
 	echo "You must have automake installed to compile libxml."
 	echo "Get ftp://ftp.cygnus.com/pub/home/tromey/automake-1.2d.tar.gz"
@@ -47,8 +47,8 @@ if test -z "$*"; then
 fi
 
 libtoolize --copy --force
-aclocal $ACLOCAL_FLAGS
-automake --add-missing
+aclocal-1.4 $ACLOCAL_FLAGS
+automake-1.4 --add-missing
 autoconf
 
 cd $THEDIR

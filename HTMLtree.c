abe01744426f83c44faec18aef7f3fead4696e5e
Daniel Veillard <veillard@src.gnome.org>
2002-09-26 12:40:03 +0000

    fixing bug #94241 on HTML boolean attributes Daniel
    * HTMLtree.c: fixing bug #94241 on HTML boolean attributes
    Daniel

--- HTMLtree.c
+++ HTMLtree.c
@@ -306,7 +306,7 @@ htmlIsBooleanAttr(const xmlChar *name)
     int i = 0;
 
     while (htmlBooleanAttrs[i] != NULL) {
-        if (xmlStrcmp((const xmlChar *)htmlBooleanAttrs[i], name) == 0)
+        if (xmlStrcasecmp((const xmlChar *)htmlBooleanAttrs[i], name) == 0)
             return 1;
         i++;
     }

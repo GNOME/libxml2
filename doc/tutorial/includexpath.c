bc6734a5c50a520a6e7dfce7f1fafd08ad6ea707
MDT 2003 John Fleck <jfleck@inkstain.net>
2003-08-28 15:01:40 +0000

    add: doc/tutorial/api.html doc/tutorial/ar01s09.html
    Thu Aug 28 08:59:51 MDT 2003 John Fleck <jfleck@inkstain.net>
    
    	add:
    	* doc/tutorial/api.html
    	* doc/tutorial/ar01s09.html
    	* doc/tutorial/includexpath.c
    	updated
    	* doc/tutorial/*.html
    	fix my bad - forgot to check in new files when I last
    	updated

---
+++ doc/tutorial/includexpath.c
@@ -0,0 +1,65 @@
+<![CDATA[
+#include <libxml/parser.h>
+#include <libxml/xpath.h>
+
+xmlDocPtr
+getdoc (char *docname) {
+	xmlDocPtr doc;
+	doc = xmlParseFile(docname);
+	
+	if (doc == NULL ) {
+		fprintf(stderr,"Document not parsed successfully. \n");
+		return NULL;
+	}
+
+	return doc;
+}
+
+xmlXPathObjectPtr
+getnodeset (xmlDocPtr doc, xmlChar *xpath){
+	
+	xmlXPathContextPtr context;
+	xmlXPathObjectPtr result;
+
+	context = xmlXPathNewContext(doc);
+	result = xmlXPathEvalExpression(xpath, context);
+	if(xmlXPathNodeSetIsEmpty(result->nodesetval)){
+                printf("No result\n");
+		return NULL;
+	}
+	xmlXPathFreeContext(context);
+	return result;
+}
+int
+main(int argc, char **argv) {
+
+	char *docname;
+	xmlDocPtr doc;
+	xmlChar *xpath = ("//keyword");
+	xmlNodeSetPtr nodeset;
+	xmlXPathObjectPtr result;
+	int i;
+	xmlChar *keyword;
+		
+	if (argc <= 1) {
+		printf("Usage: %s docname\n", argv[0]);
+		return(0);
+	}
+
+	docname = argv[1];
+	doc = getdoc(docname);
+	result = getnodeset (doc, xpath);
+	if (result) {
+		nodeset = result->nodesetval;
+		for (i=0; i < nodeset->nodeNr; i++) {
+			keyword = xmlNodeListGetString(doc, nodeset->nodeTab[i]->xmlChildrenNode, 1);
+		printf("keyword: %s\n", keyword);
+		}
+	}
+	xmlXPathFreeObject (result);
+	xmlFree(keyword);
+	xmlFreeDoc(doc);
+	xmlCleanupParser();
+	return (1);
+}
+]]>

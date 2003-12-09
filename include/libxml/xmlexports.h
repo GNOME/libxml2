d324ff058ebe3511fd1c343f3d360c3b87b4552e
Daniel Veillard <veillard@src.gnome.org>
2003-12-09 14:09:47 +0000

    applied patch from Kenneth Haley for compiling on Mingw see #128786 Daniel
    * include/libxml/xmlexports.h: applied patch from Kenneth Haley
      for compiling on Mingw see #128786
    Daniel

--- include/libxml/xmlexports.h
+++ include/libxml/xmlexports.h
@@ -88,7 +88,22 @@
 #endif
 
 /* Windows platform with GNU compiler (Mingw) */
-#if defined(_WIN32) && defined(__MINGW__)
+#if defined(_WIN32) && defined(__MINGW32__)
+  #undef XMLPUBFUN
+  #undef XMLPUBVAR
+  #undef XMLCALL
+  #if defined(IN_LIBXML) && !defined(LIBXML_STATIC)
+    #define XMLPUBFUN __declspec(dllexport)
+    #define XMLPUBVAR __declspec(dllexport) extern
+  #else
+    #define XMLPUBFUN
+    #if !defined(LIBXML_STATIC)
+      #define XMLPUBVAR __declspec(dllimport) extern
+    #else
+      #define XMLPUBVAR extern
+    #endif
+  #endif
+  #define XMLCALL __cdecl
   #if !defined _REENTRANT
     #define _REENTRANT
   #endif

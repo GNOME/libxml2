453e71bf1e3fa59b31f574d7748de02e1ba7546d
Daniel Veillard <veillard@src.gnome.org>
2004-04-20 17:44:46 +0000

    killing the strncmp vs. memcmp controversy and #140593 Daniel
    * parser.c: killing the strncmp vs. memcmp controversy and #140593
    Daniel

--- parser.c
+++ parser.c
@@ -7580,7 +7580,7 @@ xmlParseEndTag2(xmlParserCtxtPtr ctxt, const xmlChar *prefix,
     }
     SKIP(2);
 
-    if ((tlen > 0) && (memcmp(ctxt->input->cur, ctxt->name, tlen) == 0)) {
+    if ((tlen > 0) && (strncmp(ctxt->input->cur, ctxt->name, tlen) == 0)) {
         if (ctxt->input->cur[tlen] == '>') {
 	    ctxt->input->cur += tlen + 1;
 	    goto done;

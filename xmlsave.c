417b74d0b194eaceb01bbdb33938ed0fb48bf30a
Rob Richards <rrichard@src.gnome.org>
2006-08-15 23:14:24 +0000

    Add linefeeds to error messages allowing for consistant handling.
    * HTMLtree.c xmlsave.c: Add linefeeds to error messages allowing
      for consistant handling.

--- xmlsave.c
+++ xmlsave.c
@@ -122,19 +122,19 @@ xmlSaveErr(int code, xmlNodePtr node, const char *extra)
 
     switch(code) {
         case XML_SAVE_NOT_UTF8:
-	    msg = "string is not in UTF-8";
+	    msg = "string is not in UTF-8\n";
 	    break;
 	case XML_SAVE_CHAR_INVALID:
-	    msg = "invalid character value";
+	    msg = "invalid character value\n";
 	    break;
 	case XML_SAVE_UNKNOWN_ENCODING:
-	    msg = "unknown encoding %s";
+	    msg = "unknown encoding %s\n";
 	    break;
 	case XML_SAVE_NO_DOCTYPE:
-	    msg = "document has no DOCTYPE";
+	    msg = "document has no DOCTYPE\n";
 	    break;
 	default:
-	    msg = "unexpected error number";
+	    msg = "unexpected error number\n";
     }
     __xmlSimpleError(XML_FROM_OUTPUT, code, node, msg, extra);
 }

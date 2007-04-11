5657837103e9938b42135cb993f197d1235221e0
William M. Brack <wbrack@src.gnome.org>
2007-04-11 14:33:46 +0000

    small enhancement for quantifier range with min occurs of 0; fixes bug
    * xmlregexp.c: small enhancement for quantifier range with
      min occurs of 0; fixes bug 425542.
    
    svn path=/trunk/; revision=3597

--- xmlregexp.c
+++ xmlregexp.c
@@ -1601,6 +1601,11 @@ xmlFAGenerateTransitions(xmlRegParserCtxtPtr ctxt, xmlRegStatePtr from,
 	    atom->quant = XML_REGEXP_QUANT_ONCE;
 	    xmlRegStateAddTrans(ctxt, to, atom, to, -1, -1);
 	    break;
+	case XML_REGEXP_QUANT_RANGE: 
+	    if (atom->min == 0) {
+		xmlFAGenerateEpsilonTransition(ctxt, from, to);
+	    }
+	    break;
 	default:
 	    break;
     }

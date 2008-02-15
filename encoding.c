8e1a46d526a3b8912437e80cb977e0608eabd0d7
Daniel Veillard <veillard@src.gnome.org>
2008-02-15 07:47:26 +0000

    patch from Roumen Petrov to detect if iconv() needs a const for the second
    * config.h.in configure.in encoding.c: patch from Roumen Petrov
      to detect if iconv() needs a const for the second parameter
    Daniel
    
    svn path=/trunk/; revision=3693

--- encoding.c
+++ encoding.c
@@ -1703,7 +1703,7 @@ xmlIconvWrapper(iconv_t cd, unsigned char *out, int *outlen,
     }
     icv_inlen = *inlen;
     icv_outlen = *outlen;
-    ret = iconv(cd, (char **) &icv_in, &icv_inlen, &icv_out, &icv_outlen);
+    ret = iconv(cd, (ICONV_CONST char **) &icv_in, &icv_inlen, &icv_out, &icv_outlen);
     *inlen -= icv_inlen;
     *outlen -= icv_outlen;
     if ((icv_inlen != 0) || (ret == -1)) {

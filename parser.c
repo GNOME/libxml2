2c8c772d6858d2f153af004349607ec1d87eecd5
Rob Richards <rrichards@cdatazone.org>
2009-04-29 10:09:05 -0400

    * parser.c: do not set error code in xmlNsWarn

--- parser.c
+++ parser.c
@@ -727,8 +727,6 @@ xmlNsWarn(xmlParserCtxtPtr ctxt, xmlParserErrors error,
     if ((ctxt != NULL) && (ctxt->disableSAX != 0) &&
         (ctxt->instate == XML_PARSER_EOF))
 	return;
-    if (ctxt != NULL)
-	ctxt->errNo = error;
     __xmlRaiseError(NULL, NULL, NULL, ctxt, NULL, XML_FROM_NAMESPACE, error,
                     XML_ERR_WARNING, NULL, 0, (const char *) info1,
                     (const char *) info2, (const char *) info3, 0, 0, msg,

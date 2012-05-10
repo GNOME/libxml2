023206fc0839de485a69aeec44ac7a89fcbe8bf6
Patrick R. Gansterer <paroga@paroga.com>
2012-05-10 22:17:51 +0800

    xmllint: Build fix for endTimer if !defined(HAVE_GETTIMEOFDAY)
    For https://bugzilla.gnome.org/show_bug.cgi?id=638649
    code was broken !

--- xmllint.c
+++ xmllint.c
@@ -520,10 +520,11 @@ endTimer(char *format, ...)
      * We cannot do anything because we don't have a timing function
      */
 #ifdef HAVE_STDARG_H
+    va_list ap;
     va_start(ap, format);
     vfprintf(stderr, format, ap);
     va_end(ap);
-    fprintf(stderr, " was not timed\n", msec);
+    fprintf(stderr, " was not timed\n");
 #else
     /* We don't have gettimeofday, time or stdarg.h, what crazy world is
      * this ?!

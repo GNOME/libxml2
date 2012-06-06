8fc913fcc9d2c3fd5c76694cb4e631ca1242de37
Daniel Veillard <veillard@redhat.com>
2012-06-06 11:29:29 +0800

    Fix compilation on older Visual Studio
    For https://bugzilla.gnome.org/show_bug.cgi?id=666491
    
    Reported by Matt Budd <matt.budd@gmail.com>, the added support
    for VS 2010 broke older version 2005 and 2008 because it assumed
    some of the defines where present in all versions, fix that
    to check the version of VS

--- include/wsockcompat.h
+++ include/wsockcompat.h
@@ -41,7 +41,7 @@
 #define EWOULDBLOCK             WSAEWOULDBLOCK
 #define ESHUTDOWN               WSAESHUTDOWN
 
-#ifndef _MSC_VER
+#if (!defined(_MSC_VER) || (_MSC_VER < 1600))
 #define EINPROGRESS             WSAEINPROGRESS
 #define EALREADY                WSAEALREADY
 #define ENOTSOCK                WSAENOTSOCK

713434d2309da469d64b35e163ea6556dadccada
Daniel Veillard <veillard@redhat.com>
2012-09-26 10:21:06 +0800

    Silence a clang warning
    as reported by Hans Wennborg <hans@chromium.org>

--- xpath.c
+++ xpath.c
@@ -12399,7 +12399,7 @@ xmlXPathNodeCollectAndTest(xmlXPathParserContextPtr ctxt,
 				break;
 			}
 		    } else if (cur->type == type) {
-			if (type == XML_NAMESPACE_DECL)
+			if (cur->type == XML_NAMESPACE_DECL)
 			    XP_TEST_HIT_NS
 			else
 			    XP_TEST_HIT

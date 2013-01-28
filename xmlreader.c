eea38159be421dbafbee38f40e239f91734bc713
Daniel Veillard <veillard@redhat.com>
2013-01-28 16:55:30 +0100

    Cleanup on duplicate test expressions
    As pointed out by Thomas Jarosch <thomas.jarosch@intra2net.com>
    
    Daniel

--- xmlreader.c
+++ xmlreader.c
@@ -1409,8 +1409,7 @@ get_next_node:
 #endif
 	    (reader->entNr == 0) &&
 	    (reader->node->prev != NULL) &&
-            (reader->node->prev->type != XML_DTD_NODE) &&
-	    (reader->entNr == 0)) {
+            (reader->node->prev->type != XML_DTD_NODE)) {
 	    xmlNodePtr tmp = reader->node->prev;
 	    if ((tmp->extra & NODE_IS_PRESERVED) == 0) {
 		xmlUnlinkNode(tmp);
@@ -1459,8 +1458,7 @@ get_next_node:
 #endif
 	    (reader->entNr == 0) &&
 	    (oldnode->type != XML_DTD_NODE) &&
-	    ((oldnode->extra & NODE_IS_PRESERVED) == 0) &&
-	    (reader->entNr == 0)) {
+	    ((oldnode->extra & NODE_IS_PRESERVED) == 0)) {
 	    xmlUnlinkNode(oldnode);
 	    xmlTextReaderFreeNode(reader, oldnode);
 	}

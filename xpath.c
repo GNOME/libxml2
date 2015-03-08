f6aaabce85baf48d622cb4686df810fdba3db073
Nick Wellnhofer <wellnhofer@aevum.de>
2015-03-08 16:05:26 +0100

    Allow attributes on descendant-or-self axis
    If the context node is an attribute, the attribute itself is on the
    descendant-or-self axis. The principal node type of this axis is element,
    so the only node test that can return the attribute is "node()". In other
    words, "@attr/descendant-or-self::node()" is equivalent to "@attr".
    
    This matches the behavior of Saxon-CE.

--- xpath.c
+++ xpath.c
@@ -7933,14 +7933,14 @@ xmlXPathNextDescendant(xmlXPathParserContextPtr ctxt, xmlNodePtr cur) {
 xmlNodePtr
 xmlXPathNextDescendantOrSelf(xmlXPathParserContextPtr ctxt, xmlNodePtr cur) {
     if ((ctxt == NULL) || (ctxt->context == NULL)) return(NULL);
-    if (cur == NULL) {
-	if (ctxt->context->node == NULL)
-	    return(NULL);
-	if ((ctxt->context->node->type == XML_ATTRIBUTE_NODE) ||
-	    (ctxt->context->node->type == XML_NAMESPACE_DECL))
-	    return(NULL);
+    if (cur == NULL)
         return(ctxt->context->node);
-    }
+
+    if (ctxt->context->node == NULL)
+        return(NULL);
+    if ((ctxt->context->node->type == XML_ATTRIBUTE_NODE) ||
+        (ctxt->context->node->type == XML_NAMESPACE_DECL))
+        return(NULL);
 
     return(xmlXPathNextDescendant(ctxt, cur));
 }

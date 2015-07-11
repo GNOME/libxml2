GNOME/libxml2

commit ba58f23c60862f2158b457f4d30031761bf4dde1

Fix order of root nodes
Make sure root nodes are sorted before other nodes.

xpath.c
@@ -361,13 +361,13 @@ turtle_comparison:
     /*
      * compute depth to root
      */
-    for (depth2 = 0, cur = node2;cur->parent != NULL;cur = cur->parent) {
+    for (depth2 = 0, cur = node2; cur != NULL; cur = cur->parent) {
 	if (cur == node1)
 	    return(1);
 	depth2++;
     }
     root = cur;
-    for (depth1 = 0, cur = node1;cur->parent != NULL;cur = cur->parent) {
+    for (depth1 = 0, cur = node1; cur != NULL; cur = cur->parent) {
 	if (cur == node2)
 	    return(-1);
 	depth1++;

commit 3eaedba1b64180668fdab7ad2eba549586017bf3

Fix previous change to node sort order
Commit ba58f23 broke comparison of nodes from different documents.
Thanks to Olli Pottonen for the report.

xpath.c
@@ -361,14 +361,14 @@ turtle_comparison:
     /*
      * compute depth to root
      */
-    for (depth2 = 0, cur = node2; cur != NULL; cur = cur->parent) {
-	if (cur == node1)
+    for (depth2 = 0, cur = node2; cur->parent != NULL; cur = cur->parent) {
+	if (cur->parent == node1)
 	    return(1);
 	depth2++;
     }
     root = cur;
-    for (depth1 = 0, cur = node1; cur != NULL; cur = cur->parent) {
-	if (cur == node2)
+    for (depth1 = 0, cur = node1; cur->parent != NULL; cur = cur->parent) {
+	if (cur->parent == node2)
 	    return(-1);
 	depth1++;
     }
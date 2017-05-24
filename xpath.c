938835e763277684274ac31afc08fc40fa419aae
Nick Wellnhofer <wellnhofer@aevum.de>
2017-05-25 01:21:57 +0200

    Don't change context node in xmlXPathRoot

--- xpath.c
+++ xpath.c
@@ -8477,9 +8477,8 @@ void
 xmlXPathRoot(xmlXPathParserContextPtr ctxt) {
     if ((ctxt == NULL) || (ctxt->context == NULL))
 	return;
-    ctxt->context->node = (xmlNodePtr) ctxt->context->doc;
     valuePush(ctxt, xmlXPathCacheNewNodeSet(ctxt->context,
-	ctxt->context->node));
+	(xmlNodePtr) ctxt->context->doc));
 }
 
 /************************************************************************

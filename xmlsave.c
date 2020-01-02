c9faa29259ac23b5fbf945f61056288e413dae81
Nick Wellnhofer <wellnhofer@aevum.de>
2020-01-02 14:12:39 +0100

    Fix overflow check in xmlNodeDump
    Store return value of xmlBufNodeDump in a size_t before checking for
    integer overflow.
    
    Found by lgtm.com

--- xmlsave.c
+++ xmlsave.c
@@ -2187,7 +2187,7 @@ xmlNodeDump(xmlBufferPtr buf, xmlDocPtr doc, xmlNodePtr cur, int level,
             int format)
 {
     xmlBufPtr buffer;
-    int ret;
+    size_t ret;
 
     if ((buf == NULL) || (cur == NULL))
         return(-1);

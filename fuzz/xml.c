9086988ffa8da62c25c764a146a84603629734aa
Nick Wellnhofer <wellnhofer@aevum.de>
2020-12-16 15:41:52 +0100

    Enforce maximum length of fuzz input
    Remove the libfuzzer max_len option which doesn't apply to other
    fuzzing engines. Enforce the maximum length directly in the fuzz
    targets. For the xml target, lower the maximum when expanding entities
    to avoid timeout and OOM errors.

--- fuzz/xml.c
+++ fuzz/xml.c
@@ -29,13 +29,18 @@ LLVMFuzzerTestOneInput(const char *data, size_t size) {
     xmlTextReaderPtr reader;
     xmlChar *out;
     const char *docBuffer, *docUrl;
-    size_t docSize, consumed, chunkSize;
+    size_t maxSize, docSize, consumed, chunkSize;
     int opts, outSize;
 
     xmlFuzzDataInit(data, size);
     opts = xmlFuzzReadInt();
-    /* XML_PARSE_HUGE still causes timeouts. */
-    opts &= ~XML_PARSE_HUGE;
+
+    /* Lower maximum size when processing entities for now. */
+    maxSize = opts & XML_PARSE_NOENT ? 50000 : 500000;
+    if (size > maxSize) {
+        xmlFuzzDataCleanup();
+        return(0);
+    }
 
     xmlFuzzReadEntities();
     docBuffer = xmlFuzzMainEntity(&docSize);

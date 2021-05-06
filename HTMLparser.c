GNOME/libxml2

commit b25acce858d4eea49b02b5d4e32708b914107b29

Speed up htmlTagLookup
Switch to binary search. This is the first time bsearch is used in the
libxml2 code base. But it's a standard library function since C89 and
should be portable.

HTMLparser.c
@@ -1269,6 +1269,14 @@ htmlInitAutoClose(void) {
     htmlStartCloseIndexinitialized = 1;
 }
 
+static int
+htmlCompareTags(const void *key, const void *member) {
+    const char *tag = (const char *) key;
+    const htmlElemDesc *desc = (const htmlElemDesc *) member;
+
+    return(strcmp(tag, desc->name));
+}
+
 /**
  * htmlTagLookup:
  * @tag:  The tag name in lowercase
@@ -1279,14 +1287,12 @@ htmlInitAutoClose(void) {
  */
 const htmlElemDesc *
 htmlTagLookup(const xmlChar *tag) {
-    unsigned int i;
+    if (tag == NULL)
+        return(NULL);
 
-    for (i = 0; i < (sizeof(html40ElementTable) /
-                     sizeof(html40ElementTable[0]));i++) {
-        if (!xmlStrcasecmp(tag, BAD_CAST html40ElementTable[i].name))
-	    return((htmlElemDescPtr) &html40ElementTable[i]);
-    }
-    return(NULL);
+    return((const htmlElemDesc *) bsearch(tag, html40ElementTable,
+                sizeof(html40ElementTable) / sizeof(htmlElemDesc),
+                sizeof(htmlElemDesc), htmlCompareTags));
 }
 
 /**

commit 7279d236364739a05657a8a614c15990eb08d0c6

Fix htmlTagLookup
Fix regression introduced with b25acce8. Some users like libxslt may
call the HTML output functions on documents with uppercase tag names,
so we must keep case-insensitive string comparison.

Fixes #248.

HTMLparser.c
@@ -1418,10 +1418,10 @@ htmlInitAutoClose(void) {
 
 static int
 htmlCompareTags(const void *key, const void *member) {
-    const char *tag = (const char *) key;
+    const xmlChar *tag = (const xmlChar *) key;
     const htmlElemDesc *desc = (const htmlElemDesc *) member;
 
-    return(strcmp(tag, desc->name));
+    return(xmlStrcasecmp(tag, BAD_CAST desc->name));
 }
 
 /**
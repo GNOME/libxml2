742320c20f1ac5e9c2730055fd894eb330b78a48
Gowtham95ars <gowtham95ars@gmail.com>
2021-06-30 18:42:14 +0530

    Patch to set IO error on file not found
    Currently whenever a file is not present for reading, NULL is simply returned after calling xmlCheckFilename() without setting any errors, thus not catching any errors in error callbacks. This makes it difficult to understand what caused the file opening to fail. Calling xmlIOErr(0, path) on it’s negative scenario, sets the error appropriately so we can handle the file not found scenario effectively.

--- xmlIO.c
+++ xmlIO.c
@@ -859,8 +859,8 @@ xmlFileOpen_real (const char *filename) {
 
     /* Do not check DDNAME on zOS ! */
 #if !defined(__MVS__)
-    if (!xmlCheckFilename(path)){
-    	xmlIOErr(0, path);
+    if (!xmlCheckFilename(path)) {
+        xmlIOErr(0, path);    	
         return(NULL);
     }
 #endif

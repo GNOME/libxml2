08c32ea3b9d1e191612fa46c20de3169a16d5ed5
Chun-wei Fan <fanchunwei@src.gnome.org>
2021-07-22 14:46:48 +0800

    tstmem.py: Try importing from libxmlmods.libxml2mod if needed
    Distutils builds place libxml2mod.pyd under the libxmlmods subdir, so try this
    directory if 'import libxml2mod' failed.

--- python/tests/tstmem.py
+++ python/tests/tstmem.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python -u
 import libxml2
-import libxml2mod
+
+try:
+    import libxml2mod
+except ModuleNotFoundError:
+    from libxmlmods import libxml2mod
+
 import sys
 
 def error(msg, data):

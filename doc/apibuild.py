d4f90698f4a310a3800063317463200055aa5c11
Nick Wellnhofer <wellnhofer@aevum.de>
2022-03-02 17:35:39 +0100

    Add XML_DEPRECATED to ignored keywords
    Fixes the API documentation generator.

--- doc/apibuild.py
+++ doc/apibuild.py
@@ -81,6 +81,7 @@ ignored_words = {
   "LIBXML_ATTR_FORMAT": (5, "macro for gcc printf args checking extension"),
   "LIBXML_ATTR_ALLOC_SIZE": (3, "macro for gcc checking extension"),
   "ATTRIBUTE_NO_SANITIZE": (3, "macro keyword"),
+  "XML_DEPRECATED": (0, "macro keyword"),
 }
 
 def escape(raw):

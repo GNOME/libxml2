d83dc542f8cf4c7547882d8925f719bbe4c587bd
Nick Wellnhofer <wellnhofer@aevum.de>
2022-08-20 16:19:34 +0200

    Fix xmlCtxtReadDoc with encoding
    xmlCtxtReadDoc used to create an input stream involving
    xmlNewStringInputStream. This would create a stream without an input
    buffer, causing problems with encodings (see #34).
    
    After commit aab584dc3, an error was returned even with UTF-8 encodings
    which happened to work before.
    
    Make xmlCtxtReadDoc call xmlCtxtReadMemory which doesn't suffer from
    these issues. Also fix htmlCtxtReadDoc.
    
    Fixes #397.

--- HTMLparser.c
+++ HTMLparser.c
@@ -7116,22 +7116,13 @@ htmlDocPtr
 htmlCtxtReadDoc(htmlParserCtxtPtr ctxt, const xmlChar * cur,
                const char *URL, const char *encoding, int options)
 {
-    xmlParserInputPtr stream;
+    const char *buf;
 
     if (cur == NULL)
         return (NULL);
-    if (ctxt == NULL)
-        return (NULL);
-    xmlInitParser();
-
-    htmlCtxtReset(ctxt);
-
-    stream = xmlNewStringInputStream(ctxt, cur);
-    if (stream == NULL) {
-        return (NULL);
-    }
-    inputPush(ctxt, stream);
-    return (htmlDoRead(ctxt, URL, encoding, options, 1));
+    buf = (const char *) cur;
+    return (htmlCtxtReadMemory(ctxt, buf, strlen(buf), URL, encoding,
+                               options));
 }
 
 /**

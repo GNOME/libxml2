920753c4aaa66f6500ecd5b4b3e7bc3c92bf503d
Nick Wellnhofer <wellnhofer@aevum.de>
2022-08-22 13:46:50 +0200

    Don't use default SAX handler to report unrelated errors

--- catalog.c
+++ catalog.c
@@ -891,11 +891,7 @@ xmlParseCatalogFile(const char *filename) {
 
     ctxt = xmlNewParserCtxt();
     if (ctxt == NULL) {
-#ifdef LIBXML_SAX1_ENABLED
-	if (xmlDefaultSAXHandler.error != NULL) {
-	    xmlDefaultSAXHandler.error(NULL, "out of memory\n");
-	}
-#endif
+        xmlCatalogErrMemory("allocating parser context");
 	return(NULL);
     }
 

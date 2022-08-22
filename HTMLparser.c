80bd34c3c650bd68e3c9c3e2308ac1988067ad50
Nick Wellnhofer <wellnhofer@aevum.de>
2022-08-22 14:06:37 +0200

    Don't initialize SAX handler in htmlReadMemory
    The SAX handler is already initialized when creating the parser
    context.

--- HTMLparser.c
+++ HTMLparser.c
@@ -6995,9 +6995,6 @@ htmlReadMemory(const char *buffer, int size, const char *URL, const char *encodi
     ctxt = htmlCreateMemoryParserCtxt(buffer, size);
     if (ctxt == NULL)
         return (NULL);
-    htmlDefaultSAXHandlerInit();
-    if (ctxt->sax != NULL)
-        memcpy(ctxt->sax, &htmlDefaultSAXHandler, sizeof(xmlSAXHandlerV1));
     return (htmlDoRead(ctxt, URL, encoding, options, 0));
 }
 

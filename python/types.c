GNOME/libxml2

commit 75cdb6dd55c07d35ddc405ed66c8361c6922c5b9

Fix unused variable warning in python/types.c

python/types.c
@@ -82,8 +82,11 @@ libxml_PyFileGet(PyObject *f) {
     FILE *res;
     const char *mode;
 
-    int fd = PyObject_AsFileDescriptor(f);
+#ifdef _WIN32
     intptr_t w_fh = -1;
+#else
+    int fd = PyObject_AsFileDescriptor(f);
+#endif
 
 #ifdef _WIN32
     HMODULE hntdll = NULL;

commit 71736b4e9a8fb458841c05495c805ddf89027700

Fix libxml_PyFileGet
Broken in commit 75cdb6dd.

python/types.c
@@ -81,14 +81,10 @@ libxml_PyFileGet(PyObject *f) {
     int flags;
     FILE *res;
     const char *mode;
+    int fd = PyObject_AsFileDescriptor(f);
 
 #ifdef _WIN32
     intptr_t w_fh = -1;
-#else
-    int fd = PyObject_AsFileDescriptor(f);
-#endif
-
-#ifdef _WIN32
     HMODULE hntdll = NULL;
     IO_STATUS_BLOCK status_block;
     FILE_ACCESS_INFORMATION ai;
861466bf4e8dea285c644f859fd9facb03de78f7
Nick Wellnhofer <wellnhofer@aevum.de>
2022-09-06 20:32:08 +0200

    gitlab-ci: Only install MinGW autotools if needed

--- .gitlab-ci/setup_mingw.sh
+++ .gitlab-ci/setup_mingw.sh
@@ -6,6 +6,6 @@ prefix=
 if [ -n "$MINGW_PACKAGE_PREFIX" ]; then
     prefix="${MINGW_PACKAGE_PREFIX}-"
 fi
-for module in autotools libiconv python xz zlib "$@"; do
+for module in libiconv python xz zlib "$@"; do
     pacman --noconfirm -S --needed ${prefix}$module
 done

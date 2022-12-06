3aaaf5cae64d437cf3af1bcb77594eaddcd0328c
Ross Burton <ross.burton@arm.com>
2022-12-06 17:05:14 +0000

    CI: fix CI on MinGW builds
    The XML test case tarball isn't actually compressed: the published URL
    is a .tar and fetches of the .tar.gz redirect silently to the .tar,
    which is then passed to gzip which refuses to decompress uncompressed
    data.
    
    Fetch the .tar as that is the documented URL, and remove the
    decompression.

--- .gitlab-ci/setup_mingw.sh
+++ .gitlab-ci/setup_mingw.sh
@@ -6,12 +6,12 @@ prefix=
 if [ -n "$MINGW_PACKAGE_PREFIX" ]; then
     prefix="${MINGW_PACKAGE_PREFIX}-"
 fi
-for module in libiconv python xz zlib "$@"; do
+for module in libiconv python "$@"; do
     pacman --noconfirm -S --needed ${prefix}$module
 done
 
 if [ ! -e libxml2-build/xmlconf ]; then
     mkdir -p libxml2-build
-    wget https://www.w3.org/XML/Test/xmlts20080827.tar.gz -O - |
-       tar -x -z -C libxml2-build
+    wget https://www.w3.org/XML/Test/xmlts20080827.tar -O - |
+       tar -x -C libxml2-build
 fi

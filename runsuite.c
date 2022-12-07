b92768cd6247f94749a4816fcaff94abec448b56
Nick Wellnhofer <wellnhofer@aevum.de>
2022-12-08 00:24:53 +0100

    tests: Enable "runsuite" test
    This enables some tests with testcases in
    
    - test/xsdtest
    - test/relaxng/OASIS/spectest.xml
    - test/relaxng/testsuite.xml
    
    The XML Schema Test Suite will also be run it was downloaded, see
    xstc/Makefile.am. Gitlab CI should be updated to fetch these files.
    
    There are 10 expected errors in the XSD test suite. This seems to be the
    case since at least version 2.9.0 from 2012.

--- runsuite.c
+++ runsuite.c
@@ -1049,13 +1049,18 @@ main(int argc ATTRIBUTE_UNUSED, char **argv ATTRIBUTE_UNUSED) {
     old_tests = nb_tests;
     old_leaks = nb_leaks;
     xsdTest();
-    if ((nb_errors == old_errors) && (nb_leaks == old_leaks))
-	printf("Ran %d tests, no errors\n", nb_tests - old_tests);
-    else
-	printf("Ran %d tests, %d errors, %d leaks\n",
-	       nb_tests - old_tests,
-	       nb_errors - old_errors,
-	       nb_leaks - old_leaks);
+    printf("Ran %d tests, %d errors, %d leaks\n",
+           nb_tests - old_tests,
+           nb_errors - old_errors,
+           nb_leaks - old_leaks);
+    if (nb_errors - old_errors == 10) {
+        printf("10 errors were expected\n");
+        nb_errors = old_errors;
+    } else {
+        printf("10 errors were expected, got %d errors\n",
+               nb_errors - old_errors);
+        nb_errors = old_errors + 1;
+    }
     old_errors = nb_errors;
     old_tests = nb_tests;
     old_leaks = nb_leaks;

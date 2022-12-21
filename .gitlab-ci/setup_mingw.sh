4c763dd0925fb6db5766d7267ae6eb13d0c4257e
Nick Wellnhofer <wellnhofer@aevum.de>
2022-12-21 22:20:43 +0100

    gitlab-ci: Revert accidental change to setup_mingw.sh
    Commit 3aaaf5ca shouldn't have changed this line. We need these
    libraries for a full libxml2 build.

--- .gitlab-ci/setup_mingw.sh
+++ .gitlab-ci/setup_mingw.sh
@@ -6,7 +6,7 @@ prefix=
 if [ -n "$MINGW_PACKAGE_PREFIX" ]; then
     prefix="${MINGW_PACKAGE_PREFIX}-"
 fi
-for module in libiconv python "$@"; do
+for module in libiconv python xz zlib "$@"; do
     pacman --noconfirm -S --needed ${prefix}$module
 done
 

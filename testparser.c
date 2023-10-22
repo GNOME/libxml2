a9ada18352713c9ed2865b4d95a02d7abb1cd999
Nick Wellnhofer <wellnhofer@aevum.de>
2023-10-22 13:56:55 +0200

    tests: Start with testparser.c for extra tests
    Several issues require customized tests. Start with a test that push
    parses large documents. See #539.

---
+++ testparser.c
@@ -0,0 +1,45 @@
+/*
+ * testparser.c: Additional parser tests
+ *
+ * See Copyright for the status of this software.
+ */
+
+#include <libxml/parser.h>
+
+#ifdef LIBXML_PUSH_ENABLED
+static int
+testHugePush(void) {
+    xmlParserCtxtPtr ctxt;
+    int err, i;
+
+    ctxt = xmlCreatePushParserCtxt(NULL, NULL, NULL, 0, NULL);
+
+    /*
+     * Push parse a document larger than XML_MAX_LOOKUP_LIMIT
+     * (10,000,000 bytes). This mainly tests whether shrinking the
+     * buffer works when push parsing.
+     */
+    xmlParseChunk(ctxt, "<doc>", 5, 0);
+    for (i = 0; i < 1000000; i++)
+        xmlParseChunk(ctxt, "<elem>text</elem>", 17, 0);
+    xmlParseChunk(ctxt, "</doc>", 6, 1);
+
+    err = ctxt->wellFormed ? 0 : 1;
+    xmlFreeDoc(ctxt->myDoc);
+    xmlFreeParserCtxt(ctxt);
+
+    return err;
+}
+#endif
+
+int
+main(void) {
+    int err = 0;
+
+#ifdef LIBXML_PUSH_ENABLED
+    err |= testHugePush();
+#endif
+
+    return err;
+}
+

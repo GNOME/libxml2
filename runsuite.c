GNOME/libxml2

commit 9b5cce7a71cf57ef5354ca143bb1946bb77a830a

include: Remove more unnecessary includes

runsuite.c
@@ -6,9 +6,8 @@
  * daniel@veillard.com
  */
 
-#include "libxml.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 

runtest.c
@@ -11,14 +11,14 @@
  * daniel@veillard.com
  */
 
-#include "libxml.h"
+#include "config.h"
 #include <stdio.h>
-
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #elif defined (_WIN32)
 #include <io.h>
 #endif
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -69,7 +69,6 @@
 #include <libxml/threads.h>
 #include <libxml/parser.h>
 #include <libxml/catalog.h>
-#include <string.h>
 #endif
 
 /*

runxmlconf.c
@@ -6,8 +6,8 @@
  * daniel@veillard.com
  */
 
-#include "libxml.h"
 #include <stdio.h>
+#include <libxml/xmlversion.h>
 
 #ifdef LIBXML_XPATH_ENABLED
 
@@ -592,9 +592,9 @@ main(int argc ATTRIBUTE_UNUSED, char **argv ATTRIBUTE_UNUSED) {
 }
 
 #else /* ! LIBXML_XPATH_ENABLED */
-#include <stdio.h>
 int
 main(int argc ATTRIBUTE_UNUSED, char **argv) {
     fprintf(stderr, "%s need XPath support\n", argv[0]);
+    return(0);
 }
 #endif

testapi.c
@@ -11,9 +11,7 @@
 /* Disable deprecation warnings */
 #define XML_DEPRECATED
 
-#include "libxml.h"
 #include <stdio.h>
-
 #include <stdlib.h>
 #include <string.h>
 #include <libxml/xmlerror.h>

testrecurse.c
@@ -10,9 +10,9 @@
  * daniel@veillard.com
  */
 
-#include "libxml.h"
 #include <stdio.h>
 
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 

HTMLparser.c
@@ -23,7 +23,6 @@
 #include <libxml/HTMLtree.h>
 #include <libxml/entities.h>
 #include <libxml/encoding.h>
-#include <libxml/valid.h>
 #include <libxml/xmlIO.h>
 #include <libxml/uri.h>
 

HTMLtree.c
@@ -19,7 +19,6 @@
 #include <libxml/HTMLparser.h>
 #include <libxml/HTMLtree.h>
 #include <libxml/entities.h>
-#include <libxml/valid.h>
 #include <libxml/xmlerror.h>
 #include <libxml/parserInternals.h>
 #include <libxml/uri.h>

SAX2.c
@@ -23,7 +23,6 @@
 #include <libxml/xmlerror.h>
 #include <libxml/debugXML.h>
 #include <libxml/xmlIO.h>
-#include <libxml/SAX.h>
 #include <libxml/uri.h>
 #include <libxml/valid.h>
 #include <libxml/HTMLtree.h>

commit f92ea2ccecdb94cabd40c4df1a2e7f35dd7bd4cb

tests: Fix build with older MSVC
Regressed in 9b5cce7a.

Fixes #620.

runsuite.c
@@ -6,6 +6,7 @@
  * daniel@veillard.com
  */
 
+#include "config.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>

runtest.c
@@ -11,7 +11,7 @@
  * daniel@veillard.com
  */
 
-#include "config.h"
+#include "libxml.h"
 #include <stdio.h>
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>

runxmlconf.c
@@ -6,6 +6,7 @@
  * daniel@veillard.com
  */
 
+#include "config.h"
 #include <stdio.h>
 #include <libxml/xmlversion.h>
 

testapi.c
@@ -11,6 +11,7 @@
 /* Disable deprecation warnings */
 #define XML_DEPRECATED
 
+#include "config.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>

testrecurse.c
@@ -10,6 +10,7 @@
  * daniel@veillard.com
  */
 
+#include "config.h"
 #include <stdio.h>
 
 #include <stdlib.h>
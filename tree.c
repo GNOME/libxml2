GNOME/libxml2

commit f960c60d18375b423eb01973730557468e5942ee

tree: Make namespace comparison more consistent
The API allows NULL namespace URIs, so we should match them
consistently. Simply use xmlStrEqual which already takes NULL strings
into account.

tree.c
@@ -745,13 +745,11 @@ xmlNewNs(xmlNodePtr node, const xmlChar *href, const xmlChar *prefix) {
 	} else {
 	    xmlNsPtr prev = node->nsDef;
 
-	    if (((prev->prefix == NULL) && (cur->prefix == NULL)) ||
-		(xmlStrEqual(prev->prefix, cur->prefix)))
+	    if (xmlStrEqual(prev->prefix, cur->prefix))
                 goto error;
 	    while (prev->next != NULL) {
 	        prev = prev->next;
-		if (((prev->prefix == NULL) && (cur->prefix == NULL)) ||
-		    (xmlStrEqual(prev->prefix, cur->prefix)))
+		if (xmlStrEqual(prev->prefix, cur->prefix))
                     goto error;
 	    }
 	    prev->next = cur;
@@ -6089,26 +6087,15 @@ xmlSearchNs(xmlDocPtr doc ATTRIBUTE_UNUSED, xmlNodePtr node,
 	if (node->type == XML_ELEMENT_NODE) {
 	    cur = node->nsDef;
 	    while (cur != NULL) {
-		if ((cur->prefix == NULL) && (nameSpace == NULL) &&
-		    (cur->href != NULL))
-		    return(cur);
-		if ((cur->prefix != NULL) && (nameSpace != NULL) &&
-		    (cur->href != NULL) &&
-		    (xmlStrEqual(cur->prefix, nameSpace)))
+		if (xmlStrEqual(cur->prefix, nameSpace))
 		    return(cur);
 		cur = cur->next;
 	    }
 	    if (orig != node) {
 	        cur = node->ns;
-	        if (cur != NULL) {
-		    if ((cur->prefix == NULL) && (nameSpace == NULL) &&
-		        (cur->href != NULL))
-		        return(cur);
-		    if ((cur->prefix != NULL) && (nameSpace != NULL) &&
-		        (cur->href != NULL) &&
-		        (xmlStrEqual(cur->prefix, nameSpace)))
-		        return(cur);
-	        }
+	        if ((cur != NULL) &&
+                    (xmlStrEqual(cur->prefix, nameSpace)))
+		    return(cur);
 	    }
 	}
 	node = node->parent;
@@ -6177,7 +6164,7 @@ xmlSearchNsByHref(xmlDocPtr doc ATTRIBUTE_UNUSED, xmlNodePtr node,
     xmlNodePtr orig = node;
     int is_attr;
 
-    if ((node == NULL) || (node->type == XML_NAMESPACE_DECL) || (href == NULL))
+    if ((node == NULL) || (node->type == XML_NAMESPACE_DECL))
         return (NULL);
 
     if (xmlStrEqual(href, XML_XML_NAMESPACE))
@@ -6192,8 +6179,7 @@ xmlSearchNsByHref(xmlDocPtr doc ATTRIBUTE_UNUSED, xmlNodePtr node,
         if (node->type == XML_ELEMENT_NODE) {
             cur = node->nsDef;
             while (cur != NULL) {
-                if ((cur->href != NULL) && (href != NULL) &&
-                    (xmlStrEqual(cur->href, href))) {
+                if (xmlStrEqual(cur->href, href)) {
 		    if (((!is_attr) || (cur->prefix != NULL)) &&
 		        (xmlNsInScope(NULL, orig, node, cur->prefix) == 1))
 			return (cur);
@@ -6203,8 +6189,7 @@ xmlSearchNsByHref(xmlDocPtr doc ATTRIBUTE_UNUSED, xmlNodePtr node,
             if (orig != node) {
                 cur = node->ns;
                 if (cur != NULL) {
-                    if ((cur->href != NULL) && (href != NULL) &&
-                        (xmlStrEqual(cur->href, href))) {
+                    if (xmlStrEqual(cur->href, href)) {
 			if (((!is_attr) || (cur->prefix != NULL)) &&
 		            (xmlNsInScope(NULL, orig, node, cur->prefix) == 1))
 			    return (cur);

commit 7c5daa376343738a4bd85474386c976d3839f114

tree: Ignore namespace with NULL href in xmlSearchNs
Some users set href to NULL to unset a namespace without deleting it.

Also change the duplicate check in xmlNewNs which must agree with
xmlSearchNs.

Short-lived regression from f960c60d.

tree.c
@@ -746,11 +746,13 @@ xmlNewNs(xmlNodePtr node, const xmlChar *href, const xmlChar *prefix) {
 	} else {
 	    xmlNsPtr prev = node->nsDef;
 
-	    if (xmlStrEqual(prev->prefix, cur->prefix))
+	    if ((xmlStrEqual(prev->prefix, cur->prefix)) &&
+                (prev->href != NULL))
                 goto error;
 	    while (prev->next != NULL) {
 	        prev = prev->next;
-		if (xmlStrEqual(prev->prefix, cur->prefix))
+		if ((xmlStrEqual(prev->prefix, cur->prefix)) &&
+                    (prev->href != NULL))
                     goto error;
 	    }
 	    prev->next = cur;
@@ -5993,7 +5995,8 @@ xmlSearchNsSafe(xmlNodePtr node, const xmlChar *prefix,
     while ((node != NULL) && (node->type == XML_ELEMENT_NODE)) {
         cur = node->nsDef;
         while (cur != NULL) {
-            if (xmlStrEqual(cur->prefix, prefix)) {
+            if ((xmlStrEqual(cur->prefix, prefix)) &&
+                (cur->href != NULL)) {
                 *out = cur;
                 return(0);
             }
@@ -6002,7 +6005,8 @@ xmlSearchNsSafe(xmlNodePtr node, const xmlChar *prefix,
         if (orig != node) {
             cur = node->ns;
             if ((cur != NULL) &&
-                (xmlStrEqual(cur->prefix, prefix))) {
+                (xmlStrEqual(cur->prefix, prefix)) &&
+                (cur->href != NULL)) {
                 *out = cur;
                 return(0);
             }
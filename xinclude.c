1ff484339e98b9adc992478f2786c3db174c8a32
Nick Wellnhofer <wellnhofer@aevum.de>
2024-06-17 12:54:46 +0200

    xinclude: Load included documents with XML_PARSE_DTDLOAD
    We must detect ID attributes declared in external DTDs.
    
    Regressed in 12f0bb94.

--- xinclude.c
+++ xinclude.c
@@ -341,7 +341,11 @@ xmlXIncludeParseFile(xmlXIncludeCtxtPtr ctxt, const char *URL) {
 	xmlDictReference(pctxt->dict);
     }
 
-    xmlCtxtUseOptions(pctxt, ctxt->parseFlags);
+    /*
+     * We set DTDLOAD to make sure that ID attributes declared in
+     * external DTDs are detected.
+     */
+    xmlCtxtUseOptions(pctxt, ctxt->parseFlags | XML_PARSE_DTDLOAD);
 
     inputStream = xmlLoadResource(pctxt, URL, NULL, XML_RESOURCE_XINCLUDE);
     if (inputStream == NULL)

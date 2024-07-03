4a0d74dcc32ad18b9bf5f61a07c53e8649f4beb9
Nick Wellnhofer <wellnhofer@aevum.de>
2024-07-03 11:55:38 +0200

    SAX2: Reenable 'directory' as base URI fallback
    Apparently, some users overwrite this member manually to set a base URI
    for memory streams.
    
    Fixes #753.

--- SAX2.c
+++ SAX2.c
@@ -412,6 +412,13 @@ xmlSAX2ResolveEntity(void *ctx, const xmlChar *publicId, const xmlChar *systemId
     if (ctxt->input != NULL)
 	base = BAD_CAST ctxt->input->filename;
 
+    /*
+     * We don't really need the 'directory' struct member, but some
+     * users set it manually to a base URI for memory streams.
+     */
+    if (base == NULL)
+        base = BAD_CAST ctxt->directory;
+
     if ((xmlStrlen(systemId) > XML_MAX_URI_LENGTH) ||
         (xmlStrlen(base) > XML_MAX_URI_LENGTH)) {
         xmlFatalErr(ctxt, XML_ERR_RESOURCE_LIMIT, "URI too long");
@@ -573,6 +580,13 @@ xmlSAX2EntityDecl(void *ctx, const xmlChar *name, int type,
                 break;
             }
         }
+
+        /*
+         * We don't really need the 'directory' struct member, but some
+         * users set it manually to a base URI for memory streams.
+         */
+        if (base == NULL)
+            base = ctxt->directory;
 
         res = xmlBuildURISafe(systemId, (const xmlChar *) base, &URI);
 

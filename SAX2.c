GNOME/libxml2

commit 4ff2dccf9f3d653f29d27cfbae418931387d7a50

SAX2: Warn if URI resolution failed

SAX2.c
@@ -406,6 +406,7 @@ xmlSAX2ResolveEntity(void *ctx, const xmlChar *publicId, const xmlChar *systemId
     xmlParserInputPtr ret = NULL;
     xmlChar *URI;
     const xmlChar *base = NULL;
+    int res;
 
     if (ctx == NULL) return(NULL);
     if (ctxt->input != NULL)
@@ -416,8 +417,13 @@ xmlSAX2ResolveEntity(void *ctx, const xmlChar *publicId, const xmlChar *systemId
         xmlFatalErr(ctxt, XML_ERR_RESOURCE_LIMIT, "URI too long");
         return(NULL);
     }
-    if (xmlBuildURISafe(systemId, base, &URI) < 0) {
-        xmlSAX2ErrMemory(ctxt);
+    res = xmlBuildURISafe(systemId, base, &URI);
+    if (URI == NULL) {
+        if (res < 0)
+            xmlSAX2ErrMemory(ctxt);
+        else
+            xmlWarnMsg(ctxt, XML_ERR_INVALID_URI,
+                       "Can't resolve URI: %s\n", systemId);
         return(NULL);
     }
     if (xmlStrlen(URI) > XML_MAX_URI_LENGTH) {
@@ -568,11 +574,16 @@ xmlSAX2EntityDecl(void *ctx, const xmlChar *name, int type,
             }
         }
 
-        if (xmlBuildURISafe(systemId, (const xmlChar *) base, &URI) < 0) {
-            xmlSAX2ErrMemory(ctxt);
-            return;
-        }
-        if (xmlStrlen(URI) > XML_MAX_URI_LENGTH) {
+        res = xmlBuildURISafe(systemId, (const xmlChar *) base, &URI);
+
+        if (URI == NULL) {
+            if (res < 0) {
+                xmlSAX2ErrMemory(ctxt);
+            } else {
+                xmlWarnMsg(ctxt, XML_ERR_INVALID_URI,
+                           "Can't resolve URI: %s\n", systemId);
+            }
+        } else if (xmlStrlen(URI) > XML_MAX_URI_LENGTH) {
             xmlFatalErr(ctxt, XML_ERR_RESOURCE_LIMIT, "URI too long");
             xmlFree(URI);
         } else {

parser.c
@@ -12168,6 +12168,8 @@ xmlParseCtxtExternalEntity(xmlParserCtxtPtr ctxt, const xmlChar *URL,
  * @ID:  the System ID for the entity to load
  * @lst:  the return value for the set of parsed nodes
  *
+ * DEPRECATED: Use xmlParseCtxtExternalEntity.
+ *
  * Parse an external general entity
  * An external general parsed entity is well-formed if it matches the
  * production labeled extParsedEnt.

parserInternals.c
@@ -1844,9 +1844,7 @@ xmlNewEntityInputStream(xmlParserCtxtPtr ctxt, xmlEntityPtr ent) {
         input = xmlLoadExternalEntity((char *) ent->URI,
                                       (char *) ent->ExternalID, ctxt);
     } else {
-        input = xmlNewInputMemory(ctxt, NULL, "", 0, NULL,
-                                  XML_INPUT_BUF_STATIC |
-                                  XML_INPUT_BUF_ZERO_TERMINATED);
+        return(NULL);
     }
 
     if (input == NULL)

commit 5c9abbf831beb25aa0724a694e1014b13bcca5da

SAX2: Fix xmlSAX2ResolveEntity if systemId is NULL
Passing a NULL systemId results in snprintf("%s", NULL) which crashes on
some platforms. Regressed with commit 4ff2dccf.

Note that systemId should never be NULL during normal parsing. It can
only be NULL if API functions are called with a NULL systemId.

Should fix #825.

SAX2.c
@@ -396,41 +396,47 @@ xmlSAX2ResolveEntity(void *ctx, const xmlChar *publicId,
 {
     xmlParserCtxtPtr ctxt = (xmlParserCtxtPtr) ctx;
     xmlParserInputPtr ret = NULL;
-    xmlChar *URI;
-    const xmlChar *base = NULL;
-    int res;
+    xmlChar *URI = NULL;
 
     if (ctx == NULL) return(NULL);
-    if (ctxt->input != NULL)
-	base = BAD_CAST ctxt->input->filename;
 
-    /*
-     * We don't really need the 'directory' struct member, but some
-     * users set it manually to a base URI for memory streams.
-     */
-    if (base == NULL)
-        base = BAD_CAST ctxt->directory;
+    if (systemId != NULL) {
+        const xmlChar *base = NULL;
+        int res;
 
-    if ((xmlStrlen(systemId) > XML_MAX_URI_LENGTH) ||
-        (xmlStrlen(base) > XML_MAX_URI_LENGTH)) {
-        xmlFatalErr(ctxt, XML_ERR_RESOURCE_LIMIT, "URI too long");
-        return(NULL);
+        if (ctxt->input != NULL)
+            base = BAD_CAST ctxt->input->filename;
+
+        /*
+         * We don't really need the 'directory' struct member, but some
+         * users set it manually to a base URI for memory streams.
+         */
+        if (base == NULL)
+            base = BAD_CAST ctxt->directory;
+
+        if ((xmlStrlen(systemId) > XML_MAX_URI_LENGTH) ||
+            (xmlStrlen(base) > XML_MAX_URI_LENGTH)) {
+            xmlFatalErr(ctxt, XML_ERR_RESOURCE_LIMIT, "URI too long");
+            return(NULL);
+        }
+        res = xmlBuildURISafe(systemId, base, &URI);
+        if (URI == NULL) {
+            if (res < 0)
+                xmlSAX2ErrMemory(ctxt);
+            else
+                xmlWarnMsg(ctxt, XML_ERR_INVALID_URI,
+                           "Can't resolve URI: %s\n", systemId);
+            return(NULL);
+        }
+        if (xmlStrlen(URI) > XML_MAX_URI_LENGTH) {
+            xmlFatalErr(ctxt, XML_ERR_RESOURCE_LIMIT, "URI too long");
+            xmlFree(URI);
+            return(NULL);
+        }
     }
-    res = xmlBuildURISafe(systemId, base, &URI);
-    if (URI == NULL) {
-        if (res < 0)
-            xmlSAX2ErrMemory(ctxt);
-        else
-            xmlWarnMsg(ctxt, XML_ERR_INVALID_URI,
-                       "Can't resolve URI: %s\n", systemId);
-        return(NULL);
-    }
-    if (xmlStrlen(URI) > XML_MAX_URI_LENGTH) {
-        xmlFatalErr(ctxt, XML_ERR_RESOURCE_LIMIT, "URI too long");
-    } else {
-        ret = xmlLoadResource(ctxt, (const char *) URI,
-                              (const char *) publicId, XML_RESOURCE_DTD);
-    }
+
+    ret = xmlLoadResource(ctxt, (const char *) URI,
+                          (const char *) publicId, XML_RESOURCE_DTD);
 
     xmlFree(URI);
     return(ret);
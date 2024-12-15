2042090b45a530d988d7543fe124f23a9e9f41fe
Nick Wellnhofer <wellnhofer@aevum.de>
2024-12-15 22:32:12 +0100

    tree: Check reallocations for overflow

--- tree.c
+++ tree.c
@@ -45,6 +45,7 @@
 #include "private/buf.h"
 #include "private/entities.h"
 #include "private/error.h"
+#include "private/memory.h"
 #include "private/tree.h"
 
 /*
@@ -4732,8 +4733,8 @@ xmlChar *
 xmlGetNodePath(const xmlNode *node)
 {
     const xmlNode *cur, *tmp, *next;
-    xmlChar *buffer = NULL, *temp;
-    size_t buf_len;
+    xmlChar *buffer = NULL;
+    size_t buf_len, len;
     xmlChar *buf;
     const char *sep;
     const char *name;
@@ -4930,23 +4931,36 @@ xmlGetNodePath(const xmlNode *node)
         /*
          * Make sure there is enough room
          */
-        if (xmlStrlen(buffer) + sizeof(nametemp) + 20 > buf_len) {
-            buf_len =
-                2 * buf_len + xmlStrlen(buffer) + sizeof(nametemp) + 20;
-            temp = (xmlChar *) xmlRealloc(buffer, buf_len);
+        len = strlen((const char *) buffer);
+        if (buf_len - len < sizeof(nametemp) + 20) {
+            xmlChar *temp;
+            int newSize;
+
+            if ((buf_len > SIZE_MAX / 2) ||
+                (2 * buf_len > SIZE_MAX - len - sizeof(nametemp) - 20)) {
+                xmlFree(buf);
+                xmlFree(buffer);
+                return (NULL);
+            }
+            newSize = 2 * buf_len + len + sizeof(nametemp) + 20;
+
+            temp = xmlRealloc(buffer, newSize);
             if (temp == NULL) {
                 xmlFree(buf);
                 xmlFree(buffer);
                 return (NULL);
             }
             buffer = temp;
-            temp = (xmlChar *) xmlRealloc(buf, buf_len);
+
+            temp = xmlRealloc(buf, newSize);
             if (temp == NULL) {
                 xmlFree(buf);
                 xmlFree(buffer);
                 return (NULL);
             }
             buf = temp;
+
+            buf_len = newSize;
         }
         if (occur == 0)
             snprintf((char *) buf, buf_len, "%s%s%s",
@@ -5843,16 +5857,26 @@ xmlGetNsListSafe(const xmlDoc *doc ATTRIBUTE_UNUSED, const xmlNode *node,
                 if (i >= nbns) {
                     if (nbns >= maxns) {
                         xmlNsPtr *tmp;
+                        int newSize;
 
-                        maxns = maxns ? maxns * 2 : 10;
-                        tmp = (xmlNsPtr *) xmlRealloc(namespaces,
-                                                      (maxns + 1) *
-                                                      sizeof(xmlNsPtr));
+                        newSize = xmlGrowCapacity(maxns, sizeof(tmp[0]),
+                                                  10, XML_MAX_ITEMS);
+                        if (newSize < 0) {
+                            xmlFree(namespaces);
+                            return(-1);
+                        }
+#ifdef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
+                        if (newSize < 2)
+                            newSize = 2;
+#endif
+                        tmp = xmlRealloc(namespaces,
+                                         (newSize + 1) * sizeof(tmp[0]));
                         if (tmp == NULL) {
                             xmlFree(namespaces);
                             return(-1);
                         }
                         namespaces = tmp;
+                        maxns = newSize;
                     }
                     namespaces[nbns++] = cur;
                     namespaces[nbns] = NULL;
@@ -6273,6 +6297,24 @@ typedef struct {
     xmlNsPtr newNs;
 } xmlNsCache;
 
+static int
+xmlGrowNsCache(xmlNsCache **cache, int *capacity) {
+    xmlNsCache *tmp;
+    int newSize;
+
+    newSize = xmlGrowCapacity(*capacity, sizeof(tmp[0]),
+                              10, XML_MAX_ITEMS);
+    if (newSize < 0)
+        return(-1);
+    tmp = xmlRealloc(*cache, newSize * sizeof(tmp[0]));
+    if (tmp == NULL)
+        return(-1);
+    *cache = tmp;
+    *capacity = newSize;
+
+    return(0);
+}
+
 /**
  * xmlReconciliateNs:
  * @doc:  the document
@@ -6323,19 +6365,10 @@ xmlReconciliateNs(xmlDocPtr doc, xmlNodePtr tree) {
 		    /*
 		     * check if we need to grow the cache buffers.
 		     */
-		    if (sizeCache <= nbCache) {
-                        xmlNsCache *tmp;
-                        size_t newSize = sizeCache ? sizeCache * 2 : 10;
-
-			tmp = xmlRealloc(cache, newSize * sizeof(tmp[0]));
-		        if (tmp == NULL) {
-                            ret = -1;
-			} else {
-                            cache = tmp;
-                            sizeCache = newSize;
-                        }
-		    }
-		    if (nbCache < sizeCache) {
+		    if ((sizeCache <= nbCache) &&
+                        (xmlGrowNsCache(&cache, &sizeCache) < 0)) {
+                        ret = -1;
+		    } else {
                         cache[nbCache].newNs = n;
                         cache[nbCache++].oldNs = node->ns;
                     }
@@ -6367,21 +6400,10 @@ xmlReconciliateNs(xmlDocPtr doc, xmlNodePtr tree) {
 			    /*
 			     * check if we need to grow the cache buffers.
 			     */
-			    if (sizeCache <= nbCache) {
-                                xmlNsCache *tmp;
-                                size_t newSize = sizeCache ?
-                                        sizeCache * 2 : 10;
-
-                                tmp = xmlRealloc(cache,
-                                        newSize * sizeof(tmp[0]));
-                                if (tmp == NULL) {
-                                    ret = -1;
-                                } else {
-                                    cache = tmp;
-                                    sizeCache = newSize;
-                                }
-			    }
-			    if (nbCache < sizeCache) {
+                            if ((sizeCache <= nbCache) &&
+                                (xmlGrowNsCache(&cache, &sizeCache) < 0)) {
+                                ret = -1;
+                            } else {
                                 cache[nbCache].newNs = n;
                                 cache[nbCache++].oldNs = attr->ns;
 			    }
@@ -7388,9 +7410,11 @@ xmlDOMWrapNSNormAddNsMapItem2(xmlNsPtr **list, int *size, int *number,
 {
     if (*number >= *size) {
         xmlNsPtr *tmp;
-        size_t newSize;
+        int newSize;
 
-        newSize = *size ? *size * 2 : 3;
+        newSize = xmlGrowCapacity(*size, 2 * sizeof(tmp[0]), 3, XML_MAX_ITEMS);
+        if (newSize < 0)
+            return(-1);
         tmp = xmlRealloc(*list, newSize * 2 * sizeof(tmp[0]));
         if (tmp == NULL)
             return(-1);
@@ -8605,8 +8629,7 @@ xmlDOMWrapCloneNode(xmlDOMWrapCtxtPtr ctxt,
 		/*
 		* Attributes (xmlAttr).
 		*/
-                /* Use xmlRealloc to avoid -Warray-bounds warning */
-		clone = (xmlNodePtr) xmlRealloc(NULL, sizeof(xmlAttr));
+		clone = xmlMalloc(sizeof(xmlAttr));
 		if (clone == NULL)
 		    goto internal_error;
 		memset(clone, 0, sizeof(xmlAttr));

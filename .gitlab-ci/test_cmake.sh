62d4697db6268b71e36ef8fda708953cadf4082a
Nick Wellnhofer <wellnhofer@aevum.de>
2025-02-02 16:43:25 +0100

    gitlab-ci: Disable cmake:mingw for now
    Executing /mingw64/bin/cmake.exe with any arguments fails without error
    message and exit code 127 since 2025-01-21. I have no idea why.

--- .gitlab-ci/test_cmake.sh
+++ .gitlab-ci/test_cmake.sh
@@ -1,19 +1,6 @@
 #!/bin/sh
 
-echo '### 1'
-#set -e
-
-which cmake
-ls -l /mingw64/bin/cmake.exe
-file /mingw64/bin/cmake.exe
-/mingw64/bin/cmake.exe --version
-
-echo CFLAGS="-Werror $CFLAGS" \
-cmake "$@" \
-    -DBUILD_SHARED_LIBS=$BUILD_SHARED_LIBS \
-    -DCMAKE_INSTALL_PREFIX=libxml2-install \
-    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-    -S . -B libxml2-build
+set -e
 
 CFLAGS="-Werror $CFLAGS" \
 cmake "$@" \
@@ -21,9 +8,7 @@ cmake "$@" \
     -DCMAKE_INSTALL_PREFIX=libxml2-install \
     -DCMAKE_BUILD_TYPE=RelWithDebInfo \
     -S . -B libxml2-build
-echo '### 2'
 cmake --build libxml2-build --target install
-echo '### 3'
 
 (cd libxml2-build && ctest -VV)
 

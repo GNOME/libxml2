3793eaadb7187ed2aa24ab5f75e7da53d4240f82
Nick Wellnhofer <wellnhofer@aevum.de>
2025-02-16 13:54:56 +0100

    fuzz: Fix build

--- encoding.c
+++ encoding.c
@@ -1142,6 +1142,8 @@ xmlIconvFree(void *vctxt) {
 
 #if defined(FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION) && \
     defined(__GLIBC__)
+#include <libxml/parserInternals.h>
+
 static int
 xmlEncodingMatch(const char *name1, const char *name2) {
     /*

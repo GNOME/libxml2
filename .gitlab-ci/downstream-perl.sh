98776424a8fbb990f970066aeccf500973a66f64
Nick Wellnhofer <wellnhofer@aevum.de>
2025-03-09 15:13:05 +0100

    gitlab-ci: Use forked branch for XML::LibXML

--- .gitlab-ci/downstream-perl.sh
+++ .gitlab-ci/downstream-perl.sh
@@ -9,10 +9,10 @@ srcdir=$(pwd)
 incdir=$srcdir/include
 libdir=$srcdir/.libs
 
-curl -L https://cpan.metacpan.org/authors/id/S/SH/SHLOMIF/XML-LibXML-2.0210.tar.gz |tar xz
-cd XML-LibXML-2.0210
+git clone --depth 1 -b test-suite-libxml2 \
+    https://github.com/nwellnhof/perl-XML-LibXML.git
+cd perl-XML-LibXML
 
 perl Makefile.PL INC="-I$incdir" LIBS="-L$libdir -lxml2"
 make
-# Known to fail
-LD_LIBRARY_PATH=$libdir make test || true
+make test

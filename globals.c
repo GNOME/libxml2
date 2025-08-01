37bd4b284a343a18be3bc49c0f53cec5d4d1b5f9
Nick Wellnhofer <wellnhofer@aevum.de>
2025-08-01 15:30:08 +0200

    globals: Include HTMLparser.h, fixing Windows build
    Regressed with c011e760 and 85bd58ef.
    
    Fixes #961.

--- globals.c
+++ globals.c
@@ -15,6 +15,7 @@
 #include <libxml/xmlerror.h>
 #include <libxml/xmlmemory.h>
 #include <libxml/xmlIO.h>
+#include <libxml/HTMLparser.h>
 #include <libxml/parser.h>
 #include <libxml/threads.h>
 #include <libxml/tree.h>

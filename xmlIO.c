8aff3b6c72a9159e5246e0f5ac101ff7a04bc7d0
Iván Chavero <ichavero@chavero.com.mx>
2025-09-28 13:57:28 -0600

    Fix xmlOutputBufferGetContent output when encoder is set.
    The xmlOutputBufferGetContent function only returns the buffer attribute
    of the _xmlOutputBuffer struct.
    When the `encoder` attribute is set the content is stored in the `conv`
    attribute after conversions are performed and the `buffer` attribute is
    emptied. This causes the xmlOutputBufferGetContent to always return
    empty when the `encoder` is set.
    
    This patch fixes the function by returning the `conv` attribute when the
    encoder is set.
    
    Fixes: #991

--- xmlIO.c
+++ xmlIO.c
@@ -1824,8 +1824,11 @@ xmlOutputBufferCreateBuffer(xmlBuffer *buffer,
  */
 const xmlChar *
 xmlOutputBufferGetContent(xmlOutputBuffer *out) {
-    if ((out == NULL) || (out->buffer == NULL) || (out->error != 0))
+    if ((out == NULL) || (out->buffer == NULL) || ((out->encoder != NULL) && (out->conv == NULL)) || (out->error != 0))
         return(NULL);
+
+    if (out->encoder != NULL)
+        return(xmlBufContent(out->conv));
 
     return(xmlBufContent(out->buffer));
 }
@@ -1838,8 +1841,11 @@ xmlOutputBufferGetContent(xmlOutputBuffer *out) {
  */
 size_t
 xmlOutputBufferGetSize(xmlOutputBuffer *out) {
-    if ((out == NULL) || (out->buffer == NULL) || (out->error != 0))
+    if ((out == NULL) || (out->buffer == NULL) || ((out->encoder != NULL) && (out->conv == NULL)) || (out->error != 0))
         return(0);
+
+    if (out->encoder != NULL)
+        return(xmlBufUse(out->conv));
 
     return(xmlBufUse(out->buffer));
 }

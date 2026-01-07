34b3c5dd83ad750f7ad20746066631768eecc28f
emptyiscolor <emptyiscolor@gmail.com>
2026-01-07 09:27:55 +0000

    move src to expected path

--- .gitlab-ci/downstream-harnessagent.sh
+++ .gitlab-ci/downstream-harnessagent.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
-cd /src
-
 export DEBIAN_FRONTEND=noninteractive
 export FUZZING_LANGUAGE=c
 
 git clone https://github.com/zhangutah/oss-fuzz-harnessagent -b filter_libxml2 /src/oss-fuzz
 
-git clone --depth=1 https://github.com/gnome/libxml2 && \
-  cd libxml2 && apt-get update && \
-  apt-get install -y --no-install-recommends     make autoconf libtool pkg-config     zlib1g-dev liblzma-dev && \
-  curl -LO http://mirrors.kernel.org/ubuntu/pool/main/a/automake-1.16/automake_1.16.5-1.3_all.deb &&     apt install ./automake_1.16.5-1.3_all.deb && \
-  mv /src/oss-fuzz/projects/libxml2/all_harnesses/ /src/all_harnesses && \
-  cp /src/oss-fuzz/projects/libxml2/build.sh /src/build.sh && \
-  compile || exit 1
+# Copy the libxml2 repository from GitLab CI workspace to /src/libxml2
+mkdir -p /src
+cp -r "$CI_PROJECT_DIR" /src/libxml2
+cd /src/libxml2 || exit 1
+
+apt-get update && \
+apt-get install -y --no-install-recommends make autoconf libtool pkg-config zlib1g-dev liblzma-dev && \
+curl -LO http://mirrors.kernel.org/ubuntu/pool/main/a/automake-1.16/automake_1.16.5-1.3_all.deb && \
+apt install ./automake_1.16.5-1.3_all.deb && \
+mv /src/oss-fuzz/projects/libxml2/all_harnesses/ /src/all_harnesses && \
+cp /src/oss-fuzz/projects/libxml2/build.sh /src/build.sh && \
+compile || exit 1
 
 # Extract binary names from build.sh and run them
 BINARIES=$(grep -oP '(?<=-o \$OUT/)[^\s\\]+' /src/build.sh)

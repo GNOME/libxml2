10c0413044f6e051544c22a1b4223dda0845bda2
Daniel Garcia Moreno <daniel.garcia@suse.com>
2026-01-27 09:57:09 +0100

    dist: Add generated documentation to the dist
    Fix https://gitlab.gnome.org/GNOME/libxml2/-/issues/1048

--- .gitlab-ci/dist.sh
+++ .gitlab-ci/dist.sh
@@ -4,8 +4,20 @@ set -e
 
 mkdir -p libxml2-dist
 cd libxml2-dist
-sh ../autogen.sh
+sh ../autogen.sh --with-docs --with-legacy
+VERSION=$(cat libxml-2.0.pc | grep Version | cut -d" " -f2)
+
+# Build doc into the dist: https://gitlab.gnome.org/GNOME/libxml2/-/issues/1048
+cd doc
+make
+cd ..
+
 make distcheck V=1 DISTCHECK_CONFIGURE_FLAGS='--with-docs --with-legacy'
+
+tar xJvf libxml2-$VERSION.tar.xz
+cp -rf doc libxml2-$VERSION/dist-doc
+tar cJvf libxml2-$VERSION.tar.xz libxml2-$VERSION
+
 if [ -z "$CI_COMMIT_TAG" ]; then
-    mv libxml2-*.tar.xz libxml2-git-$CI_COMMIT_SHORT_SHA.tar.xz
+    mv libxml2-$VERSION.tar.xz libxml2-git-$CI_COMMIT_SHORT_SHA.tar.xz
 fi

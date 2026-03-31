7ff301bde612e1bfdbb4615c89bdd01a148b73e5
Daniel Garcia Moreno <daniel.garcia@suse.com>
2026-03-31 12:42:13 +0200

    python: Add more regexp tests
    See https://gitlab.gnome.org/GNOME/libxml2/-/issues/1086

--- python/tests/regexp.py
+++ python/tests/regexp.py
@@ -1,28 +1,35 @@
 #!/usr/bin/env python3
+import sys
 import setup_test
 import libxml2
 
 # Memory debug specific
 libxml2.debugMemory(1)
 
-re = libxml2.regexpCompile("a|b")
-if re.regexpExec("a") != 1:
-    print("error checking 'a'")
-    sys.exit(1)
-if re.regexpExec("b") != 1:
-    print("error checking 'b'")
-    sys.exit(1)
-if re.regexpExec("ab") != 0:
-    print("error checking 'ab'")
-    sys.exit(1)
-if re.regexpExec("") != 0:
-    print("error checking 'ab'")
-    sys.exit(1)
-if re.regexpIsDeterminist() != 1:
-    print("error checking determinism")
-    sys.exit(1)
-del re
-    
+
+def check_regex(regex, inside, outside):
+    re = libxml2.regexpCompile(regex)
+    for c in inside:
+        if re.regexpExec(c) != 1:
+            print("error checking inside", repr(regex), repr(c))
+            sys.exit(1)
+    for c in outside:
+        if re.regexpExec(c) != 0:
+            print("error checking outside", repr(regex), repr(c))
+            sys.exit(1)
+    if re.regexpIsDeterminist() != 1:
+        print("error checking determinism")
+        sys.exit(1)
+    del re
+
+
+check_regex("a|b", "ab", ["ab", ""])
+# https://gitlab.gnome.org/GNOME/libxml2/-/issues/1086
+check_regex(r"[\t-\r]", "\t\n\r", ["a", "(", ""])
+check_regex("[\\t-\\r]", "\t\n\r", ["a", "(", ""])
+check_regex(r"[\[-\]]", "[]\\", "Z^")
+check_regex(r"[\*-/]", "*+,-./", "()0")
+
 
 # Memory debug specific
 libxml2.cleanupParser()
